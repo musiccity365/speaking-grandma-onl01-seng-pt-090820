5b9f0316c2677359a94536e38f36b6466a99cdf5
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-11 20:33:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,10 +9,10 @@ def speak_to_grandma(phrase)
 # and yells back
   elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
   elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
   else
       return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
