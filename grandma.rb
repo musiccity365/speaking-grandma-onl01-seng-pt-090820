b9886e6a0a8b8f5cb83bb00a0858148ce55e2c20
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-11 20:32:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@ def speak_to_grandma(phrase)
   else
       return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
-
+  end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
